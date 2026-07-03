2c7d66b7beb6c845a97637c8397f9615067f3720
Wifelywax <josue.astudillo@epn.edu.ec>
2026-07-02 20:02:14 -0500

    Practica registro.c

--- src/registro.c
+++ src/registro.c
@@ -9,18 +9,20 @@
 #include <sys/types.h>
 #include "../include/registro.h"
 
+//Ubicacion de los arvchivos
 #define FIFO_RUTA "/tmp/fifo_backup_so"
 #define LOG_RUTA "./backup/historial.log"
 
 extern char ruta_base_absoluta[PATH_MAX];
 
 void crear_logger(void) {
+    //Limpiar y Crear nuevo FIFO
     unlink(FIFO_RUTA);
     if (mkfifo(FIFO_RUTA, 0666) == -1) {
         perror("Error al crear el FIFO del logger");
         exit(EXIT_FAILURE);
     }
-
+    //Creacion del Logger
     pid_t pid = fork();
 
     if (pid == -1) {
@@ -29,24 +31,25 @@ void crear_logger(void) {
     }
 
     if (pid == 0) {
+        //Abrir conducto para Lectura y Escritura
         int fd_fifo = open(FIFO_RUTA, O_RDWR);
-
+        // Construye la ruta absoluta del archivo de historial.
         char log_ruta[PATH_MAX * 2];
         snprintf(log_ruta, sizeof(log_ruta), "%s/backup/historial.log", ruta_base_absoluta);
 
-        char buffer[512];
+         char buffer[512];
         ssize_t bytes_leidos;
 
-       
-        
-        // Usamos la variable construida en lugar de LOG_RUTA
+        //Abre el archivo de historial
         int fd_log = open(log_ruta, O_WRONLY | O_CREAT | O_APPEND, 0666);
-
+        // Leer continuamente mensajes enviados por otros procesos.
         while ((bytes_leidos = read(fd_fifo, buffer, sizeof(buffer))) > 0) {
+            //Guarda en historial
             write(fd_log, buffer, bytes_leidos);
+            //Muestra en Consola
             write(STDOUT_FILENO, buffer, bytes_leidos);
         }
-
+        //Cierre de descriptores y proceso
         close(fd_fifo);
         close(fd_log);
         exit(EXIT_SUCCESS);
@@ -57,10 +60,12 @@ void registrar_evento(const char *nombre_archivo) {
     int fd_fifo = open(FIFO_RUTA, O_WRONLY);
     if (fd_fifo == -1) return;
 
+   // Obtiene la fecha y hora actual del sistema
     time_t tiempo = time(NULL);
     struct tm *tiempo_local = localtime(&tiempo);
     char mensaje[512];
-
+   
+    //Genera un mensaje con el formato:[AAAA-MM-DD HH:MM:SS]
     int longitud = snprintf( mensaje,sizeof(mensaje),
         "[%04d-%02d-%02d %02d:%02d:%02d] copiado %s\n",
         tiempo_local->tm_year + 1900,
